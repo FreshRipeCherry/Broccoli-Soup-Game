01efb89e2021f15b221c6bbdee23803aa4619ca7
Maddie <105728964+FreshRipeCherry@users.noreply.github.com>
2025-12-18 21:38:47 -0500

    Refactoring player code
    Player code refactored into a more readable format with more dedicated functions and whatnot, still works as usual.

--- Scripts Test Grounds/player.gd
+++ Scripts Test Grounds/player.gd
@@ -8,110 +8,61 @@ enum States {Normal, GndPnd}
 @export var CUR_STATE : States = States.Normal #: set = set_state
 @onready var sprite_2d = $Sprite2D
 @onready var coyote_timer = $CoyoteTimer
-@onready var brogle_collision = $CollisionShape2D # just to alter the box when needed lol
+@onready var player_collision = $CollisionShape2D # just to alter the box when needed lol
 @onready var ground_pound_hitbox = $GndPndHitbox
 
 var gravity = 5500 #ProjectSettings.get_setting("physics/2d/default_gravity")
-var squishPoppet = 0 #Variable that will squish and stretch Broccoli for some animations
-#var lastSquishGndPnd = 0 #Variable to keep track of the scale when getting stretched
-
 signal StateChanged(State : String)
 
 func _physics_process(delta):
-	#DEBUG
-	brogle_collision.set_debug_color(Color.from_hsv(257, 9, 96, 0.5))
-	brogle_collision.visible = true
-	# Disable ground pound hitbox when not in ground pound state
-	ground_pound_hitbox.monitorable = false
-	ground_pound_hitbox.monitoring = false
+	DEBUG() # DEBUG
+
 	# Get the input direction and handle the movement/deceleration.
 	var horizontal_direction = Input.get_axis("move_left", "move_right")
-	#var vertical_direction = Input.get_axis("up", "down") # Unused for now
 	if horizontal_direction:
 		velocity.x = move_toward(velocity.x, horizontal_direction*SPEED, ACCELERATION)
 	else:
 		velocity.x = move_toward(velocity.x, 0, 200) # Fixed Deceleration
-	
+
 	if horizontal_direction == -1:
 		sprite_2d.position.x = -250
 	if horizontal_direction == 1:
 		sprite_2d.position.x = 0
-	
+
 	if is_on_floor():
 		CUR_STATE = States.Normal
 		StateChanged.emit(CUR_STATE) #emits Broccoli's new state
-		#unsquish brogle
-		"""if (squishPoppet != 0):
-			sprite_2d.scale.x = squishPoppet
-			squishPoppet -= 0.085
-			sprite_2d.scale.y -= squishPoppet
-			if squishPoppet <= 0:
-				squishPoppet = 0
-				sprite_2d.scale.y = 1
-				sprite_2d.scale.x = 1
-				lastSquishGndPnd = 0"""
-		if (velocity.x > 0.1 || velocity.x < -0.1):
-			sprite_2d.animation = "upBrogleRun"
-		else:
-			sprite_2d.animation = "upBrogleIdle"
-		# EXTRA_JUMP = 1
 		if (Input.is_action_pressed("down")):
 			position.y += 1
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
-	else:  #!is_on_floor()
+	else:
 		if CUR_STATE == States.Normal:
 			velocity.y += gravity * delta
 			if velocity.y >= 3500:  #Terminal Velocity
 				velocity.y = 3500
-			if velocity.y < -1:
-				sprite_2d.animation = "upBrogleJump"
-			if velocity.y >= 0:
-				sprite_2d.animation = "upBrogleFall"
 			# Short hops!
 			if Input.is_action_just_released("jump") and velocity.y < JUMP_VELOCITY / 2:
 				velocity.y = JUMP_VELOCITY / 2
+
 			# Starts Ground Pound ability
 			if (Input.is_action_pressed("down") and Input.is_action_just_pressed("jump")):
 				velocity.y = 0 # Move the delay here
 				CUR_STATE = States.GndPnd
 				StateChanged.emit(CUR_STATE) #emits Ground Pound state
+				await get_tree().create_timer(0.24).timeout
+				velocity.y = 9500
 		elif CUR_STATE == States.GndPnd:
-			#StateChanged.emit(CUR_STATE) #emits Ground Pound state
-			if velocity.y <= 0:
-				sprite_2d.animation = "brogleGndPndStart"
-			else:
-				# Enables the ground pound hitbox once falling
-				ground_pound_hitbox.monitorable = true
-				ground_pound_hitbox.monitoring = true
-				# Updates the sprite to the falling loop
-				sprite_2d.animation = "brogleGndPndFall"
-				"""if squishPoppet >= 0.25:
-					squishPoppet = 0.25
-					sprite_2d.scale.y = lastSquishGndPnd
-					sprite_2d.scale.x = 0.65
-				lastSquishGndPnd = sprite_2d.scale.y
-				squishPoppet += 0.02
-				sprite_2d.scale.x -= squishPoppet/3
-				sprite_2d.scale.y += squishPoppet"""
-			await get_tree().create_timer(0.24).timeout
-			velocity.y = 9500
-			# Horizontal speed cap while in ground pound state.
-			if horizontal_direction:
-				velocity.x = move_toward(velocity.x, horizontal_direction*1.05, 0.6)
-			else:
-				velocity.x = move_toward(velocity.x, 0, 200)
+			groundPound(horizontal_direction)
 	
 	if coyote_timer.time_left > 0.0:
 		if (Input.is_action_pressed("down")):
 			position.y += 1
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = JUMP_VELOCITY
-		
-	if Input.is_action_pressed("move_left"):
-		sprite_2d.flip_h = true
-	if Input.is_action_pressed("move_right"):
-		sprite_2d.flip_h = false
+
+	flipSprite()
+	spriteUpdate(CUR_STATE)
 	
 	# Resetting the coyote time timer
 	var was_floored = is_on_floor()
@@ -120,14 +71,47 @@ func _physics_process(delta):
 	if no_grounded:
 		coyote_timer.start()
 	
-	# Pushing boxes
+	# Push boxes
+	pushBox()
+	
+func pushBox() -> void:
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
 		if c.get_collider() is RigidBody2D:
 			c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE)
-			
+
+func groundPound(direction) -> void:
+	if velocity.y <= 0: sprite_2d.animation = "brogleGndPndStart"
+	else:
+		# Enables the ground pound hitbox once falling
+		ground_pound_hitbox.monitorable = true
+		ground_pound_hitbox.monitoring = true
+		sprite_2d.animation = "brogleGndPndFall" # Falling loop once timer expires
+		# Horizontal speed cap while in ground pound state.
+		if direction:
+			velocity.x = move_toward(velocity.x, direction*1.05, 0.6)
+		else:
+			velocity.x = move_toward(velocity.x, 0, 200)
+
+func flipSprite() -> void:
+	if Input.is_action_pressed("move_left"):
+		sprite_2d.flip_h = true
+	if Input.is_action_pressed("move_right"):
+		sprite_2d.flip_h = false
+
+func spriteUpdate(playerState: States) -> void:
+	if playerState == States.Normal:
+		if is_on_floor():
+			sprite_2d.animation = "upBrogleIdle" if velocity.x == 0 else "upBrogleRun"
+		else: sprite_2d.animation = "upBrogleJump" if velocity.y < -1 else "upBrogleFall"
+	else: pass
+
+
+
+func DEBUG() -> void:
+	player_collision.set_debug_color(Color.from_hsv(257, 9, 96, 0.5))
+	player_collision.visible = true
+	# Disable ground pound hitbox when not in ground pound state
+	ground_pound_hitbox.monitorable = false
+	ground_pound_hitbox.monitoring = false
 	
-"""	func set_state(new_state: int) -> void:
-		var previous_state := CUR_STATE
-		CUR_STATE = new_state
-"""
